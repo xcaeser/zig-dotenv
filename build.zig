29c528416f955668a0038c61e0187f4e9d4b0eea
xcaeser <oxidase-airbase-0m@icloud.com>
2025-05-29 11:32:39 +0200

    Refactor build process to remove static library creation and ensure proper linking of libc

--- build.zig
+++ build.zig
@@ -8,25 +8,16 @@ pub fn build(b: *std.Build) void {
     // Create the module
     _ = b.addModule("dotenv", .{
         .root_source_file = b.path("src/dotenv.zig"),
+        .link_libc = true,
     });
-
-    // Add the library for consumers
-    const lib = b.addStaticLibrary(.{
-        .name = "dotenv",
-        .root_source_file = b.path("src/dotenv.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    lib.linkLibC();
-    b.installArtifact(lib);
 
     // Create tests
     const lib_test = b.addTest(.{
         .root_source_file = b.path("src/dotenv.zig"),
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
-    lib_test.linkLibC();
 
     const run_test = b.addRunArtifact(lib_test);
     run_test.has_side_effects = true;
